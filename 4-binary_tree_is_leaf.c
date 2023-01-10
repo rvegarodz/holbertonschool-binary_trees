7cdccc73f972c39f8043bac98a8c4cf05c73189d
rvegarodz <r.vega27@outlook.com>
2023-01-10 21:42:33 +0000

    adding files for task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->parent != NULL && node->left == NULL && node->right == NULL)
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	else
 		return (0);
