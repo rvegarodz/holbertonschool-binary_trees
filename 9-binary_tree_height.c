9767ce38d985c89d5fdd464e24f47f7da57a7713
rvegarodz <r.vega27@outlook.com>
2023-01-12 17:21:09 +0000

    adding files for task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,27 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t count, l_height, r_height;
+
+	count = 0;
 
 	if (tree == NULL)
 		return (count);
-	if (tree->left != NULL)
-		count++;
-	if (tree->right != NULL)
-		count++;
+	else
+	{
+		if (!tree->left && !tree->right)
+		{
+			count = 0;
+			return (count);
+		}
+
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+		if (l_height > r_height)
+			count = l_height + 1;
+		else
+			count = r_height + 1;
+	}
 	return (count);
 
 }
