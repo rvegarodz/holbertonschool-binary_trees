73f1aa82ecbb7ae97d4f550d248276f0a9472718
rvegarodz <r.vega27@outlook.com>
2023-01-13 19:38:58 +0000

    adding file for task 18, edited

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,7 +6,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL)
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
