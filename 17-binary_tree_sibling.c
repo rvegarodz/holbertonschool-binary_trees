a32a1e565b6182aef916203336ac2a06062810e4
rvegarodz <r.vega27@outlook.com>
2023-01-13 17:57:31 +0000

    adding file task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,17 +6,15 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *temp_node;
-	
-	temp_node = malloc(sizeof(binary_tree_t));
-	if (temp_node == NULL)
-		return (NULL);
-	else
-		temp_node = NULL;
 
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	else
 	{
+		temp_node = malloc(sizeof(binary_tree_t));
+		if (temp_node == NULL)
+			return (NULL);
+		
 		temp_node = node->parent;
 		if (temp_node->left == node)
 			temp_node = temp_node->right;
